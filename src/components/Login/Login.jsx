acbab39d4e27baeafad07046891a49f4f66ab763
engadnanhossain <engr.adnanhossain@gmail.com>
2023-04-28 11:28:43 +0600

    added login with firebase

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Context/UserContext';
 
 const Login = () => {
+const {signIn} = useContext(AuthContext);
+
+const handleSubmitLogin = event => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    
+    signIn(email, password)
+    .then(result => {
+        console.log(result.user);
+        form.reset();
+    })
+    .catch(error => console.error(error))
+}
+
     return (
         <div className='w-50 m-auto py-5'>
             <h2 className='my-5 text-center'>Login</h2>
-            <Form>
+            <Form onSubmit={handleSubmitLogin}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" name='email' placeholder="Enter email" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" name='password' placeholder="Password" />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Login
